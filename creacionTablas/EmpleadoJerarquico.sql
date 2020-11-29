90ff2b929fee80bf0a5fa5e6e83572254ce58655
Mariquena Sofia <mariquena2020@outlook.com.ar>
2020-11-29 19:50:27 -0300

    creaciones de tablas

---
+++ creacionTablas/EmpleadoJerarquico.sql
@@ -0,0 +1,7 @@
+CREATE TABLE EmpleadoJerarquico(
+	numeroEmpleado int PRIMARY KEY NOT NULL,
+	fechaInicio date NOT NULL,
+	numeroArea int UNIQUE NOT NULL,
+	CONSTRAINT fk_Jerarquico_nroEmpleado FOREIGN KEY(numeroEmpleado) REFERENCES Empleado(numeroEmpleado),
+	CONSTRAINT fk_Jerarquico_nroArea FOREIGN KEY(numeroArea) REFERENCES Area(numeroArea)
+)

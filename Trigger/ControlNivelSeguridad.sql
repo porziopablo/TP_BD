58a540aef0ecb8625bed4c6a0b02f1bcc25f6cfa
ivan aprea <ivanaprea.cabj@gmail.com>
2020-12-02 13:31:04 -0300

    Modificados los trigger con if exists y funcion EstaLibre

--- Trigger/ControlNivelSeguridad.sql
+++ Trigger/ControlNivelSeguridad.sql
@@ -2,20 +2,10 @@ CREATE TRIGGER ControlNivelSeguridad
 ON TIENEASIGNADO
 FOR INSERT, UPDATE
 AS 
-	declare @nivel_area INT
-	declare @nivel_empleado INT
-
-	SELECT @nivel_area = numeroNivel FROM AREA
-	JOIN inserted
-	ON inserted.numeroArea = AREA.numeroArea
-	WHERE AREA.numeroArea = inserted.numeroArea
-
-	SELECT @nivel_empleado = numeroNivel FROM EMPLEADONOPROFESIONAL
-	JOIN inserted
-	ON inserted.numeroEmpleado = EMPLEADONOPROFESIONAL.numeroEmpleado
-	WHERE EMPLEADONOPROFESIONAL.numeroEmpleado = inserted.numeroEmpleado
-
-	IF (@nivel_area != @nivel_empleado)
+	IF EXIST (SELECT inserted.*
+				FROM inserted,AREA A,EMPLEADONOPROFESIONAL ENP
+				WHERE inserted.numeroEmpleado = ENP.numeroEmpleado AND inserted.numeroArea = A.numeroArea AND 
+					ENP.numeroNivel != A.numeroNivel)
 	BEGIN
 		raiserror ('Los niveles de seguridad del Area y el Empleado no coinciden', 16, 1)
 		rollback transaction

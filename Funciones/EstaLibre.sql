cd20e1274ee68cdadc8e9109386a4641318c4b84
porziopablo <porziopablo@outlook.com>
2020-12-06 18:51:15 -0300

    Funcion EstaLibre arreglada

--- Funciones/EstaLibre.sql
+++ Funciones/EstaLibre.sql
@@ -1,44 +1,21 @@
-/*CREATE FUNCTION EstaLibre(@numeroEmpleado INT, @fechaInicio DATE)
-
-RETURNS INT
-
-AS
-	BEGIN
-		DECLARE @OK INT
-
-		IF @fechaInicio > (SELECT TOP 1 DTC.fechaFinTrabajo
-					FROM DONDETRABAJAC DTC
-					WHERE DTC.numeroEmpleado = @numeroEmpleado
-					ORDER BY DTC.fechaFinTrabajo DESC)
-			SET @OK = 1
-		ELSE
-			SET @OK = 0
-
-		RETURN @OK
-	END
-	*/
-
-CREATE FUNCTION EstaLibre(@numeroEmpleado INT, @fechaInicio DATE)
+CREATE FUNCTION estaLibre(@numeroEmpleado INT, @fechaInicio DATE)
 
 RETURNS INT
 
 AS
     BEGIN
         DECLARE @OK INT
-        DECLARE @FECHAMAX DATE
-        SET @FECHAMAX = (SELECT TOP 1 DTC.fechaFinTrabajo
-                         FROM DONDETRABAJAC DTC
-                         WHERE DTC.numeroEmpleado = @numeroEmpleado
-                         ORDER BY DTC.fechaFinTrabajo DESC)
-        IF (@@ROWCOUNTS = 0)
+
+        IF NOT EXISTS (SELECT * FROM DONDETRABAJAC DTC WHERE DTC.numeroEmpleado = @numeroEmpleado AND DTC.fechaInicioTrabajo != @fechaInicio)
             SET @OK = 1
-        ELSE IF (@fechaInicio > @FECHAMAX)
-            SET @OK = 1
-        ELSE
-            SET @OK = 0
+
+        ELSE IF @fechaInicio > (SELECT TOP 1 DTC.fechaFinTrabajo
+                        FROM DONDETRABAJAC DTC
+                        WHERE DTC.numeroEmpleado = @numeroEmpleado AND DTC.fechaInicioTrabajo != @fechaInicio
+                        ORDER BY DTC.fechaFinTrabajo DESC)
+                SET @OK = 1
+            ELSE
+                SET @OK = 0
 
         RETURN @OK
     END
-
-
-

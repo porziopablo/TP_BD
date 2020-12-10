0e1804beff2ae9fe8f8b13fb8c21d2698a34336e
Ivan <ivanaprea.cabj@gmail.com>
2020-12-10 14:15:06 -0300

    Test triggers

---
+++ Test/testMovimientoAreaBaja.sql
@@ -0,0 +1,15 @@
+INSERT INTO [dbo].[SeMueveEn]
+           ([numeroEmpleado]
+           ,[numeroArea]
+           ,[fechaHoraMov]
+           ,[resultadoMov]
+           ,[tipoMov])
+     VALUES
+	 --guarda un movimiento en un area de nivel bajo.
+           (2
+           ,1
+           ,'2020-12-01 10:12:13.3456123'
+           ,'autorizado'
+           ,'ingreso')
+
+
